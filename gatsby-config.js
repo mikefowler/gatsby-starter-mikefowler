b5dce068f052eecffd30aef46558bc471a9e0a95
Mike Fowler <mike.fowler@airbnb.com>
2019-08-27 09:03:41 -0400

    Set up support for Google Fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,17 @@ module.exports = {
       },
     },
     'gatsby-plugin-typescript',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        // https://fonts.google.com/
+        fonts: [
+          `Roboto+Condensed\:700`,
+          `Cabin`
+        ],
+        display: 'swap'
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
